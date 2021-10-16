f2267cf50f6e4437834a2559ccdd806b202ef0b7
NycGio <giorgialvarez@gmail.com>
2021-10-16 18:07:46 -0400

    Added logic to search component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,19 +1,27 @@
 import React, { Fragment } from "react";
 import { getAllEvents } from "../../dummy-data";
 
+import { useRouter } from "next/router";
+
 import EventList from "../../components/events/EventList";
 import EventSearch from "../../components/events/events-search";
 
 function EventsIndex() {
+	const router = useRouter();
 	const events = getAllEvents();
 
 	if (!events) {
 		return <p>No events found</p>;
 	}
 
+	const findEventsHandler = (year, month) => {
+		const fullPath = `/events/${year}/${month}`;
+		router.push(fullPath);
+	};
+
 	return (
 		<Fragment>
-			<EventSearch />
+			<EventSearch onSearch={findEventsHandler} />
 			<EventList items={events} />
 		</Fragment>
 	);
